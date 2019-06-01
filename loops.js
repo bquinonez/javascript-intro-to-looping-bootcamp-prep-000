388ce078b64d2e332a3fd2f5d958906be5c4146a
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-06-01 03:27:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,9 +18,19 @@ function whileLoop(n) {
     return "done";
   }
   
-function doWhileLoop() {
-  
+
+
+
+var i = 0;
+ 
+function incrementVariable() {
+  i = i + 1;
+  return i;
 }
+ 
+do {
+  console.log("doo-bee-doo-bee-doo");
+} while (incrementVariable() < 5);
    
    
    
